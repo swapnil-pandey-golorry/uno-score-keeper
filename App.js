8b35c278ff0571a0eb70648c7c5328b318c020b8
Swapnil Pandey <swapnilpandey@golorry.com>
2021-09-03 12:15:05 +0530

    setup project

---
+++ App.js
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import StartScreen from './Screens/StartScreen';
+
+const App = () => {
+  return (
+    <StartScreen></StartScreen>
+  )
+}
+
+
+export default App;
---
+++ Screens/StartScreen.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {View, Text, TextInput, StyleSheet} from 'react-native'
+
+const StartScreen = () => {
+    return (
+        <View style = {styles.screen}>
+
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        backgroundColor : '#2ECC71'
+    }
+})
+export default StartScreen
